f7c8f7738f4c0210689e8ad43324203b84ec37cb
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-10-23 16:56:11 -0300

    Create forca.h

---
+++ Projetos/forca.h
@@ -0,0 +1,11 @@
+# define TAMANHO_PALAVRA 20
+int letraexiste(char letra);
+int chuteserrados();
+int enforcou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+int ganhou();
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
