9d437de509f85d2c57d76480624795d121526343
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-17 20:18:22 -0300

    Create adivinhacao.c

---
+++ Estudos/Livro/jogoAdvinhacao/adivinhacao.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main()
+{
+  printf("************************************\n");
+  printf("* Bem-vindo ao Jogo de Adivinhacao *\n");
+  printf("************************************");
+}
