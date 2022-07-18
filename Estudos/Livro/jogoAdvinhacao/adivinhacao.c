c456c40a6e108a26d51ae99ea69b207661980144
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-18 10:12:01 -0300

    Update adivinhacao.c

--- Estudos/Livro/jogoAdvinhacao/adivinhacao.c
+++ Estudos/Livro/jogoAdvinhacao/adivinhacao.c
@@ -4,5 +4,11 @@ void main()
 {
   printf("************************************\n");
   printf("* Bem-vindo ao Jogo de Adivinhacao *\n");
-  printf("************************************");
+  printf("************************************\n");
+
+  int numeroSecreto = 42;
+  int chute;
+  printf("Qual eh o seu chute?: ");
+  scanf("%d", &chute);
+  printf("Seu chute foi %d\n", chute);
 }
