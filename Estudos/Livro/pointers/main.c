23aa5ec7b96077ff3fd40a8b24fb739dd8a4ca42
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-24 20:26:38 -0300

    Update main.c

--- Estudos/Livro/pointers/main.c
+++ Estudos/Livro/pointers/main.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
-void play(int attempts) {
-  printf("joga %d\n", attempts);
-  
-  attempts++;
-  printf("joga %d\n", attempts);
-}
-
 void main() {
-  int attempts = 0;
-  printf("main %d\n", attempts);
-  
-  attempts++;
-  printf("main %d\n", attempts);
+  int c = 10;
+  int* ponteiro;
+  //ponteiro apontando para o mesmo endereço de c
+  ponteiro = &c;
 
-  attempts++;
-  printf("main %d\n", attempts);
+  // imprime o endereço da variável c
+  printf("%d %d\n", &c, ponteiro);
 
-  play(attempts);
 
-  printf("main %d\n", attempts);
-}
+  //imprime o conteúdo da variável c
+  printf("%d %d\n", c, *ponteiro); 
+} 
