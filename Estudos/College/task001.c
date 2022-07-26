ae8f3c25500763f9651ece0895e131e65d74e0f6
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-26 09:54:05 -0300

    Create task001.c

---
+++ Estudos/College/task001.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+const char* coordPos(double coordX, double coordY) {
+  char* quadrante = "A coordenada se encontra na origem\n";
+
+  if(coordX > 0 && coordY > 0) {
+    quadrante = "A coordenada eh do primeiro quadrante\n";
+  }else if(coordX < 0 && coordY > 0) {
+    quadrante = "A coordenada eh do segundo quadrante\n";
+  }else if(coordX < 0 && coordY < 0) {
+    quadrante = "A coordenada eh do terceiro quadrante\n";
+  }else if(coordX > 0 && coordY < 0) {
+    quadrante = "A coordenada eh do quarto quadrante\n";
+  }
+
+  return quadrante;
+}
+
+void main() 
+{
+  double coordX, coordY;
+
+  printf("Digite a coodernada para o eixo X: ");  
+  scanf("%lf", &coordX);
+
+  printf("Digite a coodernada para o eixo Y: ");  
+  scanf("%lf", &coordY);
+
+  printf(coordPos(coordX, coordY));
+
+}
