9bb2da3563765e9e6a2d5d5d12253d70edbf3b1e
athena272 <guilhermera272@gmail.com>
2022-07-29 09:02:47 -0300

    Create forca.h

---
+++ Projetos/jogoForca/forca.h
@@ -0,0 +1,8 @@
+// lista de funções que aparecerão no arquivo
+int enforcou();
+void abertura();
+void chuta();
+int jaChutou(char letra);
+int ganhou();
+void desenhaForca();
+void escolhePalavra();
