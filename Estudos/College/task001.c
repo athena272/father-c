a3df3e65972a5e95a11a7dbabb6762b05a1416cf
athena272 <guilhermera272@gmail.com>
2022-07-29 19:10:28 -0300

    Update task001.c

--- Estudos/College/task001.c
+++ Estudos/College/task001.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
 
-const char* coordPos(double coordX, double coordY) {
-  char* quadrante = "A coordenada se encontra na origem\n";
+const char *coordPos(double coordX, double coordY)
+{
+  char *quadrante = "A coordenada se encontra na origem\n";
 
-  if(coordX > 0 && coordY > 0) {
+  if (coordX > 0 && coordY > 0)
+  {
     quadrante = "A coordenada eh do primeiro quadrante\n";
-  }else if(coordX < 0 && coordY > 0) {
+  }
+  else if (coordX < 0 && coordY > 0)
+  {
     quadrante = "A coordenada eh do segundo quadrante\n";
-  }else if(coordX < 0 && coordY < 0) {
+  }
+  else if (coordX < 0 && coordY < 0)
+  {
     quadrante = "A coordenada eh do terceiro quadrante\n";
-  }else if(coordX > 0 && coordY < 0) {
+  }
+  else if (coordX > 0 && coordY < 0)
+  {
     quadrante = "A coordenada eh do quarto quadrante\n";
+  }
+  else if (coordX == 0 && coordY != 0)
+  {
+    quadrante = "A coordenada esta no eixo Y\n";
+  }
+  else if (coordX != 0 && coordY == 0)
+  {
+    quadrante = "A coordenada esta no eixo X\n";
   }
 
   return quadrante;
 }
 
-void main() 
+void main()
 {
   double coordX, coordY;
 
-  printf("Digite a coodernada para o eixo X: ");  
+  printf("Digite a coodernada para o eixo X: ");
   scanf("%lf", &coordX);
 
-  printf("Digite a coodernada para o eixo Y: ");  
+  printf("Digite a coodernada para o eixo Y: ");
   scanf("%lf", &coordY);
 
   printf(coordPos(coordX, coordY));
-
 }
