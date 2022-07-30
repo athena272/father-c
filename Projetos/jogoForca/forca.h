a65d2ab6babfa2491a1bdd9829b7b9d3d330fada
athena272 <guilhermera272@gmail.com>
2022-07-30 17:38:53 -0300

    Update forca.h

--- Projetos/jogoForca/forca.h
+++ Projetos/jogoForca/forca.h
@@ -1,8 +1,13 @@
+#define TAMANHO_PALAVRA 20 
+
 // lista de funções que aparecerão no arquivo
+int letraExiste(char letra);
+int chutesErrados();
 int enforcou();
+int ganhou();
 void abertura();
 void chuta();
 int jaChutou(char letra);
-int ganhou();
 void desenhaForca();
 void escolhePalavra();
+void adicionaPalavra();
