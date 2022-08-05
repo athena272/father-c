d003dde8c5d7d0f085ea77f1d59e37173a72e176
athena272 <guilhermera272@gmail.com>
2022-08-05 14:35:04 -0300

    Update declaracao.c

--- Estudos/AndreBackesC/apostilaC/struct/declaracao.c
+++ Estudos/AndreBackesC/apostilaC/struct/declaracao.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct cadastro
+{
+  char nome[50];
+  int idade;
+  char rua[50];
+  int numero;
+};
+
+struct aluno
+{
+  char nome[50];
+  int matricula;
+  float nota1, nota2, nota3;
+};
 
 void main()
 {
-  struct cadastro 
-  {
-    char nome[50];
-    int idades;
-    char rua[50];
-    int numero;
-  };
+  struct cadastro C;
+  //Atribui a string "Carlos" para o campo nome
+  strcpy(C.nome, "Carlos"); 
 
-  struct aluno
-  {
-    char nome[50];
-    int matricula;
-    float nota1, nota2, nota3;
-  };
+  //Atribui o valor 18 para o campo idade
+  C.idade = 18;
+
+  //Atribui a string "Avenida Brasil" para o campo rua
+  strcpy(C.rua, "Avenida Brasil");
+
+  //Atribui o valor 1082 para o campo numero
+  C.numero = 1082;
+
+  
 }
