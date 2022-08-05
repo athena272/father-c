5c466e6b365e707f9e1922d1f569a204a862410b
athena272 <guilhermera272@gmail.com>
2022-08-05 04:11:52 -0300

    Create declaracao.c

---
+++ Estudos/AndreBackesC/apostilaC/struct/declaracao.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+  struct cadastro 
+  {
+    char nome[50];
+    int idades;
+    char rua[50];
+    int numero;
+  };
+
+  struct aluno
+  {
+    char nome[50];
+    int matricula;
+    float nota1, nota2, nota3;
+  };
+}
