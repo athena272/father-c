ca34849566b90c544ce047f3c87260443f10118a
athena272 <guilhermera272@gmail.com>
2022-08-06 17:54:25 -0300

    Update scan.c

--- Estudos/AndreBackesC/apostilaC/passagem/scan.c
+++ Estudos/AndreBackesC/apostilaC/passagem/scan.c
@@ -3,7 +3,7 @@
 void main()
 {
   int x = 5;
-  printf("Anntes da funcao scanf: x = %d\n", x);
+  printf("Antes da funcao scanf: x = %d\n", x);
   printf("Digite um numero: ");
   scanf("%d", &x);
   printf("Depois da funcao scanf: x = %d\n", x);
