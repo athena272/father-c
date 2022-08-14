d150beba6eff195bffeb00dc0782d8f4a4c41782
athena272 <guilhermera272@gmail.com>
2022-08-14 16:32:57 -0300

    Create byBigRef.c

---
+++ Estudos/AndreBackesC/apostilaC/passagem/byBigRef.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void troca(int *a, int *b)
+{
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
+  printf("Dentro: %d e %d\n", *a, *b);
+}
+
+void main()
+{
+  int x = 2;
+  int y = 3;
+  printf("Antes: %d e %d\n", x, y);
+  troca(&x, &y);
+  printf("Depois: %d e %d\n", x, y);
+}
